5cd07b9b81c5c30f5537e7782951aa76c6bfbda6
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-09-26 13:48:52 +0100

    let follow windows win move to another tag

--- config.def.h
+++ config.def.h
@@ -8,6 +8,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int rmaster            = 1;        /* 1 means master-area is initially on the right */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const char *fonts[]          = { "FiraCode\ Nerd\ Font:size=12" };
 static const char dmenufont[]       =   "FiraCode\ Nerd\ Font:size=12";
--- dwm.c
+++ dwm.c
@@ -1880,6 +1880,8 @@ tag(const Arg *arg)
 		selmon->sel->tags = arg->ui & TAGMASK;
 		focus(NULL);
 		arrange(selmon);
+		if(viewontag)
+			view(arg);
 	}
 }
 
