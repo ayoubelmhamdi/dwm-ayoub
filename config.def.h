ef8cbbeceb45d8c2bcb1e99243caeb38878ce3b0
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2021-09-27 02:05:55 +0100

    switch automatique to tag opened

--- config.def.h
+++ config.def.h
@@ -60,18 +60,14 @@ static const char *colors[][3]      = {
 static const char *tags[] = { "1", "2", "3" ,"4" };
 
 static const Rule rules[] = {
-	/* xprop(1):
-	 *	WM_CLASS(STRING) = instance, class
-	 *	WM_NAME(STRING) = title
-	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           -1 },
-	{ "Chromium",  NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "Brave",    NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 3,       0,           -1 },
- 	{ NULL,		  "ketty",		NULL,		SPTAG(0),		1,			 -1 },
- 	{ NULL,		  "spfm",		NULL,		SPTAG(1),		1,			 -1 },
- 	{ NULL,		  "keepassxc",	NULL,		SPTAG(2),		0,			 -1 },
+ 	/* class      instance    title       tags mask     switchtotag    isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,             0,           -1 },
+	{ "Chromium", NULL,       NULL,       1 << 3,       1,             0,           -1 },
+	{ "Brave",    NULL,       NULL,       1 << 3,       1,             0,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 3,       1,             0,           -1 },
+ 	{ NULL,		    "ketty",		NULL,		    SPTAG(0),		  0,             1,		     	  -1 },
+ 	{ NULL,		    "spfm",		  NULL,		    SPTAG(1),		  0,             1,			      -1 },
+ 	{ NULL,		    "keepassxc",NULL,		    SPTAG(2),		  0,             0,			      -1 },
 };
 
 /* layout(s) */
